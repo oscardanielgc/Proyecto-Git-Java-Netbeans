4696385358298a46576952b529a66e17a89a103d
Oscar Garzón <od6@misena.edu.co>
2021-04-09 07:45:35 -0500

    cambio saludo2

--- src/pruebagittirar/PruebaGitTirar.java
+++ src/pruebagittirar/PruebaGitTirar.java
@@ -16,7 +16,7 @@ public class PruebaGitTirar {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello Prueba Git a tirar!");
+        System.out.println("Hello Prueba Git a tirar duro!");
     }
     
 }
